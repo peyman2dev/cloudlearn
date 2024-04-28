a615c6c8c98d6107ecb946554b4914ce1ba901ab
peyman2dev <peyman2dev@gmail.com>
2024-04-28 13:36:38 +0330

    Application Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1220px",
+    },
+    extend: {
+      fontFamily: {
+        "Worksans-Thin": "Worksans-Thin",
+        "Worksans-Light": "Worksans-Light",
+        "Worksans-Regular": "Worksans-Regular",
+        "Worksans-Medium": "Worksans-Medium",
+        "Worksans-SemiBold": "Worksans-SemiBold",
+        "Worksans-Bold": "Worksans-Bold",
+        "Worksans-ExtraBold": "Worksans-ExtraBold",
+        "Worksans-Black": "Worksans-Black",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
+};
